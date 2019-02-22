c7dc425d0338531b7985b54cd2316709cd0f78e1
j2jeremy <jamesjeremyb@gmail.com>
2019-02-22 12:33:11 -0800

    added notes and my solutions

---
+++ freecodecamp-basic scripting.js
@@ -0,0 +1,214 @@
+//Reverse a String - my solution, didnt need array, stringArr could just be a string
+function reverseString(str) { 
+  let stringArr  =[];
+  for(let i = str.length-1; i>=0; i--){
+    stringArr.push(str.charAt(i));
+  }
+  return stringArr.join("");
+}
+
+//Reverse a String - #2 built in string prototype methods
+function reverseString(str) {
+    // Step 1. Use the split() method to return a new array
+    var splitString = str.split(""); // var splitString = "hello".split(""); // ["h", "e", "l", "l", "o"]
+    // Step 2. Use the reverse() method to reverse the new created array
+    var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+    // Step 3. Use the join() method to join all elements of the array into a string
+    var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join(""); // ["o", "l", "l", "e", "h"] // "olleh"
+    //Step 4. Return the reversed string
+    return joinArray; // "olleh"
+}
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+//Reverse a String #3 recursion
+function reverseString(str) {
+  if (str === "")
+    return "";
+  else
+    return reverseString(str.substr(1)) + str.charAt(0);
+}
+
+//Factorialize a Number - my solution
+function factorialize(num) {
+  return num ==0 ? 1 : num == 1 ? num : factorialize(num-1) * num;
+}
+console.log(factorialize(0)); 
+
+//Find the Longest Word in a String - my solution
+function findLongestWordLength(str) {
+  let longestWord = "";
+  let stringArr = str.split(" ");
+  //console.log(stringArr);
+  for(let word in stringArr){
+    if (stringArr[word].length > longestWord.length){
+      longestWord = stringArr[word];
+    }
+  }
+  return longestWord.length;
+}
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+
+//find the longest word in a string - intermediate solution
+function findLongestWordLength(s) {
+  return s.split(' ')
+    .reduce(function(x, y) {
+      return Math.max(x, y.length)
+    }, 0); // 0 here gives x a starting value for the math.max
+}
+
+//find the longest word in a string - advanced recursion solution
+function findLongestWordLength(str) {
+  str = str.split(" ");//split the string into individual words  //(important!!, you'll see why later)
+  if(str.length == 1){ //str only has 1 element left that is the longest element, //return the length of that element
+    return str[0].length;
+  }
+  if(str[0].length >= str[1].length){//if the first element's length is greater than the second element's (or equal)  //remove the second element and recursively call the function)
+    str.splice(1,1);
+    return findLongestWordLength(str.join(" "));
+  }
+  if(str[0].length <= str[1].length){ //if the second element's length is greater thant the first element's start //call the function past the first element 
+    // from the first element to the last element inclusive.
+    return findLongestWordLength(str.slice(1,str.length).join(" "));
+  }
+}
+
+//Return Largest Numbers in Arrays - my solution
+function largestOfFour(arr) {
+  let newArr = [];
+  function getLargestNum(array){
+   return array.reduce(function(x,y){
+     return Math.max(x,y);
+   });
+  }
+  for(let a in arr){
+    newArr.push(getLargestNum(arr[a]));
+  }
+  return newArr;
+}
+largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]);
+
+
+function largestOfFour(arr) {
+  return arr.map(function(group){ //creates a new array with the results of calling a function for every array element. //does not mutate original array
+    return group.reduce(function(prev, current) {
+      return (current > prev) ? current : prev;
+    });
+  });
+}
+
+//Return Largest Numbers in Arrays - advanced solution
+function largestOfFour(arr) {
+  return arr.map(Function.apply.bind(Math.max, null));
+}
+
+//Confirm the Ending - my solution
+function confirmEnding(str, target) {
+  let startingIndex = str.length-target.length;
+  return str.substr(startingIndex) == target;
+}
+confirmEnding("Bastian", "n");
+
+//confirm the ending - alternative
+function confirmEnding(str, target) {
+  return str.slice(str.length - target.length) === target;
+}
+confirmEnding("He has to give me a new name", "name");
+
+//Repeat a String Repeat a String - my solution
+function repeatStringNumTimes(str, num) {
+  let repeated = "";
+ if(num < 0){
+   return repeated;
+ }
+ for(let i=0; i<num; i++){
+   repeated+=str;
+ }
+ return repeated;
+}
+//repeat a string repeat a string - alternate 
+function repeatStringNumTimes(str, num) {
+  var accumulatedStr = '';
+
+  while (num > 0) {
+    accumulatedStr += str;
+    num--;
+  }
+  return accumulatedStr;
+}
+//repeat a string repeat a string -intermediate
+function repeatStringNumTimes(str, num) {
+  if(num < 0)
+    return "";
+  if(num === 1)
+    return str;
+  else
+    return str + repeatStringNumTimes(str, num - 1);
+}
+//repeat a string repeat a string -advanced
+function repeatStringNumTimes(str, num) {
+  return num > 0 ? str.repeat(num) : '';
+}
+repeatStringNumTimes("abc", 3);
+
+//Truncate a String - my solution
+function truncateString(str, num) {
+  return str.length <= num ? str : str.slice(0, num) + '...';
+}
+//truncate a string - advanced solution
+function truncateString(str, num) {
+  if (str.length <= num) {
+    return str;
+  } else {
+    return str.slice(0, num > 3 ? num - 3 : num) + '...';
+  }
+}
+//Finders Keepers - my solution
+function findElement(arr, func) {
+  for(let i in arr){
+    if(func(arr[i])){
+      return arr[i];
+    }
+  }
+  return undefined;
+} 
+
+//Boo who - my solution
+function booWho(bool) {
+  return (typeof bool == "boolean");
+}
+booWho(null);
+
+//Title Case a Sentence - my solution
+function titleCase(str) {
+  let stringArr = str.split(" ");
+  for(let word in stringArr){
+    stringArr[word] = stringArr[word].charAt(0).toUpperCase() + stringArr[word].substr(1).toLowerCase();
+  }
+  return stringArr.join(" ");
+}
+
+//title case a sentence - intermediate
+function titleCase(str) {
+  var convertToArray = str.toLowerCase().split(" ");
+  var result = convertToArray.map(function(val){
+      return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+  });
+  return result.join(" ");
+}
+//title case a senetence - advanced
+function titleCase(str) {
+  return str.toLowerCase().replace(/(^|\s)\S/g, (L) => L.toUpperCase());
+}
+
+//Slice and Splice
+function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+  let localArray = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    localArray.splice(n, 0, arr1[i]);
+    n++;
+  }
+  return localArray;
+}
---
+++ freecodecamp-basic-data-structures.js
@@ -0,0 +1,159 @@
+//Access an Array's Contents Using Bracket Notation
+let ourArray = ["a", "b", "c"];
+let ourVariable = ourArray[0]; // ourVariable equals "a"
+ourArray[1] = "not b anymore"; // ourArray now equals ["a", "not b anymore", "c"];
+
+//Add Items to an Array with push() and unshift()
+let twentyThree = 'XXIII';
+let romanNumerals = ['XXI', 'XXII'];
+romanNumerals.unshift('XIX', 'XX'); //unshift adds to front // now equals ['XIX', 'XX', 'XXI', 'XXII']
+romanNumerals.push(twentyThree); //push adds to end // now equals ['XIX', 'XX', 'XXI', 'XXII', 'XXIII']
+
+//Remove Items from an Array with pop() and shift()
+let greetings = ['whats up?', 'hello', 'see ya!'];
+greetings.pop(); //removes last ele // now equals ['whats up?', 'hello']
+greetings.shift(); //removes element from the front // now equals ['hello']
+
+// Remove Items Using splice() 
+let array = ['I', 'am', 'feeling', 'really', 'happy'];
+let newArray = array.splice(3, 2); //remove 2 eles beginning at 3rd ele, returns new array // newArray equals ['really', 'happy']
+
+//Add Items Using splice()
+function colorChange(arr, index, newColor) {
+  arr.splice(index, 1, newColor); // we have removed '#bb7e8c' and added '#332327' in its place
+  return arr;
+}
+let colorScheme = ['#878787', '#a08794', '#bb7e8c', '#c9b6be', '#d1becf'];
+colorScheme = colorChange(colorScheme, 2, '#332327');// colorScheme now equals ['#878787', '#a08794', '#332327', '#c9b6be', '#d1becf']
+
+//Copy Array Items Using slice()
+let weatherConditions = ['rain', 'snow', 'sleet', 'hail', 'clear']; // weatherConditions still equals ['rain', 'snow', 'sleet', 'hail', 'clear']
+let todaysWeather = weatherConditions.slice(1, 3); // todaysWeather equals ['snow', 'sleet'];
+
+//Copy an Array with the Spread Operator
+let thisArray = [true, true, undefined, false, null]; // thisArray remains unchanged, and is identical to thatArray
+let thatArray = [...thisArray]; // thatArray equals [true, true, undefined, false, null]
+
+//Combine Arrays with the Spread Operator
+let thisArray = ['sage', 'rosemary', 'parsley', 'thyme'];
+let thatArray = ['basil', 'cilantro', ...thisArray, 'coriander']; // thatArray now equals ['basil', 'cilantro', 'sage', 'rosemary', 'parsley', 'thyme', 'coriander']
+
+//Check For The Presence of an Element With indexOf()
+let fruits = ['apples', 'pears', 'oranges', 'peaches', 'pears'];
+fruits.indexOf('dates') // returns -1
+fruits.indexOf('oranges') // returns 2
+fruits.indexOf('pears') // returns 1, the first index at which the element exists
+
+//Iterate Through All an Array's Items Using For Loops (other methods: every(), forEach(), map() )
+function filteredArray(arr, elem) { //returns new arry containing nested arrays that don't contain the 'elem' param
+  console.log("original arr: " + arr);
+  let newArr = [];
+  for (let i = 0; i< arr.length; i++){
+    if(arr[i].indexOf(elem) == -1){
+      newArr.push(arr[i]);
+    }
+  } 
+  return newArr; 
+}  
+console.log(filteredArray([[10, 8, 3], [14, 6, 23], [3, 18, 6]], 18));
+
+//Add Key-Value Pairs to JavaScript Objects
+let FCC_User = {
+  username: 'awesome_coder',
+  followers: 572,
+  points: 1741,
+  completedProjects: 15
+};
+let userData = FCC_User.followers; // userData equals 572
+let userData = FCC_User['followers']; // userData equals 572
+FCC_User['registered'] = true;
+
+//Modify an Object Nested Within an Object
+let nestedObject = {
+  id: 28802695164,
+  date: 'December 31, 2016',
+  data: {
+    totalUsers: 99,
+    online: 80,
+    onlineStatus: {
+      active: 67,
+      away: 13
+    }
+  }
+};
+
+//Access Property Names with Bracket Notation 
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+function checkInventory(scannedItem) {
+  return foods[scannedItem];
+}
+console.log(checkInventory("apples"));
+//Use the delete Keyword to Remove Object Properties
+delete foods.apples;
+//Check if an Object has a Property
+foods.hasOwnProperty('apples');
+'apples', 'oranges' in foods;
+//Iterate Through the Keys of an Object with a for...in Statement
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+function countOnline(obj) {
+  let count=0;
+  for(let user in obj){
+     if(obj[user].online == true){
+       count++;
+     }
+  }
+  return count;
+}
+//Generate an Array of All Object Keys with Object.keys()
+return Object.keys(obj);
+//Modify an Array Stored in an Object
+let user = {
+  name: 'Kenneth',
+  age: 28,
+  data: {
+    username: 'kennethCodesAllDay',
+    joinDate: 'March 26, 2016',
+    organization: 'freeCodeCamp',
+    friends: [
+      'Sam',
+      'Kira',
+      'Tomo'
+    ],
+    location: {
+      city: 'San Francisco',
+      state: 'CA',
+      country: 'USA'
+    }
+  }
+};
+function addFriend(userObj, friend) {
+  // change code below this line  
+  userObj.data.friends.push(friend);
+  return userObj.data.friends;
+  // change code above this line
+}
